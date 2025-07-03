116b6b0d3cdb0ced5a63ad9e75a1b73690ca2d4e
juliaam <juliademoraess@gmail.com>
2025-07-02 22:39:29 -0300

    chore: configure alias

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,5 @@
+import scss from "./button.module.scss";
+
+export function Button() {
+  return <button className={scss.buttonContainer}>oi</button>;
+}
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/Button/button";
 import scss from "./home.module.scss";
 
 export function Home() {
-  return <div className={scss.container}>eu sou a home</div>;
+  return (
+    <div className={scss.container}>
+      <Button />
+    </div>
+  );
 }
